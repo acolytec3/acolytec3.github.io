6b5bbb934b2df61c48fdf60418a14c1a6af87e7c
acolytec3 <a@b.com>
2019-07-23 17:03:07 +0000

    Initial commit from gatsby: (https://github.com/anubhavsrivastava/gatsby-starter-grayscale.git)

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteTitle: 'Dev Portolio', // <title>
+  manifestName: 'Portfolio',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/portfolio/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Portfolio',
+  subHeading: 'Part-time OSS wannabe. Full time student of C^3  ',
+
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/acolytec3',
+    },
+  ],
+};
