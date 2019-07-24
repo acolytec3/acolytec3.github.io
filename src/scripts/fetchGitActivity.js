08fbe97038c0975e4ef8656af2f1f0fdd8b1e41d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2019-07-24 05:18:13 -0400

    First adds

---
+++ src/scripts/fetchGitActivity.js
@@ -0,0 +1,71 @@
+var axios = require('axios')
+var fs = require('fs')
+
+console.log(process.env.GRAPHQL_TOKEN)
+const githubUrl = 'https://api.github.com/graphql';
+
+const token = process.env.GRAPHQL_TOKEN;
+
+const oauth = {Authorization: 'bearer ' + token}
+
+const query = `{
+    user(login: "acolytec3") {
+      bio
+      avatarUrl
+      contributionsCollection {
+        pullRequestContributionsByRepository {
+          contributions(first: 100) {
+            nodes {
+              pullRequest {
+                title
+                body
+              }
+            }
+          }
+          repository {
+            name
+            url
+            description
+          }
+        }
+      }
+      repositories(first: 100, privacy: PUBLIC) {
+        nodes {
+          name
+          url
+          isFork
+          stargazers {
+            totalCount
+          }
+          owner {
+            login
+          }
+          forks {
+            totalCount
+          }
+          homepageUrl
+          description
+          repositoryTopics(first: 100) {
+            nodes {
+              topic {
+                name
+              }
+            }
+          }
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }`;
+
+  axios.post(githubUrl, {query: query}, {headers: oauth})
+  .then(function (response) {
+    fs.writeFileSync('./src/pages/gitActivity.json',JSON.stringify(response.data, null, 4))
+  })
+  .catch(function (error) {
+    // On error, print the error
+    console.log(error);
+  });
